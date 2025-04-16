c528f9856593c3f75bc05c04b69276f39fb65555
Michael Burkhart <burkh4rt@uchicago.edu>
2025-04-16 16:34:09 -0400

    proper size comparison

--- slurm/03_tune_model.sh
+++ slurm/03_tune_model.sh
@@ -4,9 +4,16 @@
 #SBATCH --output=./output/%j-%x.stdout
 #SBATCH --partition=gpuq
 #SBATCH --gres=gpu:8
-#SBATCH --time=1-00:00:00
+#SBATCH --array=0-5
 
 source preamble.sh
+
+echo "SLURM_ARRAY_JOB_ID=${SLURM_ARRAY_JOB_ID}"
+echo "SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}"
+
+names=(1B med small smol tiny teensy)
+hidden_sizes=(2048 1024 512 256 128 64)
+intermediate_sizes=(8192 2048 1024 512 256 128)
 
 torchrun --nproc_per_node=8 \
     ../src/scripts/tune_model.py \
@@ -16,10 +23,10 @@ torchrun --nproc_per_node=8 \
     --data_version QC_day_stays \
     --collation packed \
     --model_dir "${hm}/clif-mdls" \
-    --model_version llama1b-small \
+    --model_version "llama1b-${names[$SLURM_ARRAY_TASK_ID]}" \
     --model_name "meta-llama/Llama-3.2-1B" \
-    --wandb_project mimic-llama \
-    --hidden_size $((2 ** 10)) \
-    --intermediate_size $((2 ** 12)) \
+    --wandb_project llama-sizing \
+    --hidden_size "${hidden_sizes[$SLURM_ARRAY_TASK_ID]}" \
+    --intermediate_size "${intermediate_sizes[$SLURM_ARRAY_TASK_ID]}" \
     --num_hidden_layers $((2 ** 3)) \
     --num_attention_heads $((2 ** 3))
